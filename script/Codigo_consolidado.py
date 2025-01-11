93ad4e319a33c27665e02d231cca975c25ea78a1
Giuseppe Lavarello <121526026+Gaddran@users.noreply.github.com>
2025-01-11 19:28:48 -0300

    parte 2.1

--- script/Codigo_consolidado.py
+++ script/Codigo_consolidado.py
@@ -1330,6 +1330,74 @@ list_cooc = (df_agg.reset_index().merge(nodes[['lugar','id']], left_on='i', righ
 list_cooc_2 = list_cooc[list_cooc['pairs']>0]
 
 
+# #### Red base sin Filtrar
+
+# Crear un grafo bipartito
+Base = nx.Graph()
+
+# Agregar nodos de usuarios y destinos
+usuarios = df_base['id_usuario'].unique()
+destinos = df_base['place_name'].unique()
+
+Base.add_nodes_from(usuarios, bipartite=0, tipo='usuario')  # Nodos de tipo 'usuario'
+Base.add_nodes_from(destinos, bipartite=1, tipo='destino')  # Nodos de tipo 'destino'
+
+# Agregar enlaces con pesos según el número de visitas
+for _, row in df_base.iterrows():
+    usuario = row['id_usuario']
+    destino = row['place_name']
+    if Base.has_edge(usuario, destino):
+        Base[usuario][destino]['weight'] += 1
+    else:
+        Base.add_edge(usuario, destino, weight=1)
+
+# Resumen de la red
+print(f"Número total de nodos: {len(Base.nodes)}")
+print(f"Número total de enlaces: {len(Base.edges)}")
+
+
+# La red tiene una gran cantidad de nodos y enlaces que es demasiado grande para representarla gráficamente sin filtrar.
+# Por este motivo, es que exploraremos los destinos más visitados y crearemos una subred más manejable para poder entender tendencias y patrones.
+
+# Top destinos más visitados (grado más alto)
+destinos_total = sorted(
+    [(nodo, grado) for nodo, grado in Base.degree() if Base.nodes[nodo]['tipo'] == 'destino'],
+    key=lambda x: x[1],
+    reverse=True
+)
+
+
+
+# Filtrar nodos con grado mayor a un umbral 
+umbral_grado = 0
+nodos_relevantes = [n for n, grado in Base.degree() if grado > umbral_grado]
+subred = Base.subgraph(nodos_relevantes)
+
+# Crear posición específica para nodos bipartitos
+usuarios = [n for n, d in subred.nodes(data=True) if d['tipo'] == 'usuario']
+destinos = [n for n, d in subred.nodes(data=True) if d['tipo'] == 'destino']
+pos = nx.bipartite_layout(subred, usuarios)
+
+# Diferenciar nodos por tipo (colores)
+node_colors = ['blue' if n in usuarios else 'orange' for n in subred.nodes()]
+
+# Visualización
+plt.figure(figsize=(14, 10))
+nx.draw(
+    subred,
+    pos,
+    with_labels=False,
+    node_size=20,
+    node_color=node_colors,
+    edge_color='gray',
+    alpha=0.7
+)
+plt.title("Red Bipartita: Usuarios y Destinos Relevantes")
+plt.show()
+
+
+# #### Proyección Unimodal
+
 W = nx.Graph()
 for row in list_cooc_2.itertuples():
     W.add_edge(row.i, row.j, weight=row.pairs)
@@ -1344,7 +1412,6 @@ sorted_keys0 = sorted(dict10, key=dict10.get)  # [1, 3, 2]
 
 for w in sorted_keys0:
     sorted_dict0[w] = dict10[w]
-sorted_dict0
 
 
 fig, ax = plt.subplots(1, 1, figsize=(16, 8))
@@ -1435,8 +1502,6 @@ subconjunto_usuarios_activos = df_base[df_base['id_usuario'].isin(usuarios_activ
 print(f"Total de usuarios más activos: {len(usuarios_activos)}")
 print(f"Total de filas en el subconjunto: {subconjunto_usuarios_activos.shape[0]}")
 
-
-import networkx as nx
 
 # Crear un grafo bipartito
 B = nx.Graph()
@@ -1541,18 +1606,56 @@ plt.title("Proyección Unimodal Filtrada: Relación entre Usuarios")
 plt.show()
 
 
+# **COEFICIENTE DE CLUSTERING**
+
 # Calcular grado de los nodos
 grados = dict(proyeccion_filtrada.degree())
 # Ordenar por grado descendente
 grados_ordenados = sorted(grados.items(), key=lambda x: x[1], reverse=True)
-print("Top 10 nodos con mayor grado:")
-for nodo, grado in grados_ordenados[:10]:
-    print(f"Usuario: {nodo}, Grado: {grado}")
 
 
-# El top 10 de nodos con mayor grado son los usuarios que comparten destinos con más personas. Estos usuarios corresponden a los "hubs" sociales, visitando destinos populares que conectan a varias personas.
+valores = [value[1] for value in grados_ordenados]
 
-# **CENTRALIDAD DE INTERMEDIACIÒN**
+fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+
+n, bins, patches = plt.hist(valores, bins=90, facecolor='#2ab0ff', edgecolor='#e0e0e0', linewidth=0.5, alpha=0.7)
+
+n = n.astype('int') # it MUST be integer
+# Good old loop. Choose colormap of your taste
+for i in range(len(patches)):
+    patches[i].set_facecolor(plt.cm.viridis(n[i]/max(n)))
+# Make one bin stand out   
+patches[47].set_alpha(1) # Set opacity
+# Add title and labels with custom font sizes
+plt.title('Distribución de grado de la Red ', fontsize=12)
+plt.xlabel('Grado', fontsize=20)
+plt.ylabel('Cantidad de Nodos', fontsize=20)
+ax.set_title("Distribución de Grado de la Red Filtrada por peso",
+             pad=24, fontweight=700, fontsize=20)
+plt.show()
+
+
+#F0.edges(data=True)
+N10 = len(proyeccion_filtrada)
+L10 = proyeccion_filtrada.size()
+degrees10 = list(dict(proyeccion_filtrada.degree()).values())
+kmin10 = min(degrees10)
+kmax10 = max(degrees10)
+print("Número de nodos: ", N10)
+print("Número de enlaces: ", L10)
+print('-------')
+print("Grado promedio: ", 2*L10/N10) 
+print('-------')
+print("Grado mínimo: ", kmin10)
+print("Grado máximo: ", kmax10)
+print('-------')
+print('Densidad: ', nx.density(proyeccion_filtrada))
+print('Diametro: ',nx.diameter(proyeccion_filtrada))
+
+nx.draw_networkx(proyeccion_filtrada)
+
+
+# **CENTRALIDAD DE INTERMEDIACIÓN**
 
 # Calcular centralidad de intermediación
 intermediacion = nx.betweenness_centrality(proyeccion_filtrada)
@@ -1669,4 +1772,263 @@ for usuario, comunidad in df_comunidades.values:
 # Mostrar recomendaciones para los primeros 5 usuarios
 for usuario, destinos in list(recomendaciones.items())[:5]:
     print(f"Usuario {usuario}: Recomendaciones: {destinos[:5]}")
+
+
+# #### B. Subconjunto filtrado por correlación
+
+# Esta sección presenta un análisis detallado de la matriz de co-ocurrencia.
+# 
+# Primero, se transforma el DataFrame `list_cooc` en un formato de matriz, donde tanto las filas como las columnas representan los distintos elementos. Los valores en las celdas de la matriz indican la frecuencia de co-ocurrencia entre los elementos. Los elementos diagonales, que representan los auto-enlaces, son descartados (ajustados a cero) para excluirlos del análisis.
+# 
+# En segundo lugar, se crean varias matrices vacías (`phi`, `tval`, `pval`, `phi2` y `exit`) con el propósito de almacenar diferentes medidas estadísticas. La matriz `phi` se utiliza para almacenar los coeficientes de correlación phi, `tval` para los t-values, `pval` para los valores p (que indican la significancia estadística), `phi2` almacena los pesos finales, y `exit` es una matriz binaria que marca si una correlación específica es significativa según los umbrales definidos por `pval_limit` y `cnt_limit`.
+# 
+# A continuación, se ejecuta un bucle anidado para calcular los coeficientes de correlación phi entre cada par de elementos. Este coeficiente se calcula tomando en cuenta el número total de observaciones y las frecuencias de co-ocurrencia de los elementos. En los casos en que el denominador de la fórmula es cero, el coeficiente phi se asigna a cero para evitar errores de división por cero. Después, se calculan los valores t y p para evaluar la significancia de las correlaciones. Si una correlación cumple con los criterios de significancia establecidos (es decir, si el valor p es menor que `pval_limit` y el número de co-ocurrencias es mayor que `cnt_limit`), la matriz `exit` marca esa correlación como significativa (valor de 1), y su coeficiente phi se almacena en la matriz `phi2`.
+# 
+# Una vez calculadas las medidas estadísticas, se construyen DataFrames para almacenar los coeficientes phi y los valores p. Estos DataFrames se combinan con los datos originales de co-ocurrencia para formar un DataFrame final, el cual incluye tanto el coeficiente phi como el valor p y el número de co-ocurrencias entre cada par de elementos.
+# 
+# Finalmente, el análisis visualiza la estructura de la red en función de diferentes umbrales de valores p. Se genera un rango de valores p y se filtra el DataFrame final para incluir únicamente aquellos pares con valores p inferiores a cada umbral. Luego, se calcula y grafica el número de nodos únicos y aristas, mostrando cómo cambia la estructura de la red a medida que se ajustan los niveles de significancia. Los gráficos incluyen líneas verticales que indican umbrales específicos de valores p como referencia para facilitar la interpretación de los resultados.
+
+from tqdm import tqdm
+from scipy import stats
+# Matriz de Co-Ocurrencia
+od_matrix = list_cooc.set_index(['i', 'j']).unstack().fillna(0)
+indices = od_matrix.index
+columns = od_matrix.columns
+od_matrix = od_matrix.values
+
+# eliminamos la diagonal (decisión de análisis: no nos interesan los auto-enlaces)
+np.fill_diagonal(od_matrix, 0)
+
+
+n = len(od_matrix)
+phi = np.zeros((n,n)) #matrix to store the phi correlations
+tval = np.zeros((n,n))
+pval = np.zeros((n,n)) # Significancia
+phi2 = np.zeros((n,n)) # Pesos finales
+exit = np.zeros((n,n)) # Matriz con unos
+
+
+
+M = od_matrix.sum(axis=1) # total number of obervations of each JUEGO (suma sobre las filas)
+T = sum(M) # Total number of observations
+
+pval_limit = 1 # significancia limite, 1 significa no filtrar
+cnt_limit = 0 # minimo numero de coincidencias a considerar en la od_matrix, 0 significa no filtrar
+
+
+# El mitico doble for
+for i in tqdm(range(n), desc="Calculando Correlaciones"):# https://github.com/tqdm/tqdm  (barra de progreso)
+    for j in range(n):
+        numerador = T * od_matrix[i, j] - M[i] * M[j]
+        denominador = np.sqrt(M[i] * M[j] * (T - M[i]) * (T - M[j]))
+        phi[i, j] = numerador / denominador if denominador != 0 else 0
+            
+        if phi[i, j] != 0:
+            sqrt_arg = max(0, max(M[i], M[j]) - 2)
+            t_numerador = phi[i, j] * np.sqrt(sqrt_arg)
+            t_denominador = np.sqrt(1 - phi[i, j]**2)
+            tval[i, j] = t_numerador / t_denominador if t_denominador != 0 else 0
+            pval[i, j] = stats.t.sf(np.abs(tval[i, j]), max(M[i], M[j]) - 2) * 2
+            # Se evaluan los criterios para ver si la correlacion es significativa
+            if pval[i, j] <= pval_limit and od_matrix[i, j] > cnt_limit:
+                exit[i, j] = 1
+                phi2[i, j] = phi[i, j]
+            else:
+                exit[i, j] = 0
+                phi2[i, j] = 0
+
+# Creamos un dataframe con toda la inforamcion estadistica de la red
+
+df_edgelist_phi  = pd.DataFrame(phi, index=indices, columns=columns).stack(future_stack=True).reset_index()
+df_edgelist_phi.columns = ['source', 'target', 'phi']# esto es por convención, recuerden que es este grafo es no-dirigido
+df_edgelist_phi = df_edgelist_phi[df_edgelist_phi['source']<=df_edgelist_phi['target']] # eliminamos duplicados
+
+df_edgelist_pval = pd.DataFrame(pval, index=indices, columns=columns).stack(future_stack=True).reset_index()
+df_edgelist_pval.columns = ['source', 'target', 'pval']
+df_edgelist_pval = df_edgelist_pval[df_edgelist_pval['source']<=df_edgelist_pval['target']] # eliminamos duplicados
+
+
+# El DataFrame final contiene el valor phi, el pval y el número de veces que ocurre que dos IDs i y j están conectados
+df = df_edgelist_phi.merge(df_edgelist_pval).merge(list_cooc.rename(columns={'i': 'source', 'j': 'target'}), how='left')
+df['pairs'] = df['pairs'].fillna(0).astype(int) 
+df = df[df['pairs']>0]
+
+
+# Visualización del número de nodos y aristas según un filtro en pval
+pvalues = np.arange(0.01,1.01,0.01)#Return evenly spaced values within a given interval.
+total_nodes, total_edges = [], []
+
+for p in pvalues:
+    __df = df.pipe(lambda x : x[x['pval']<=p])
+    n_nodes = pd.concat([__df['source'], __df['target']]).nunique()
+    total_nodes.append(n_nodes)
+    total_edges.append(len(__df))
+pval_df = pd.DataFrame({'pval': pvalues, 'nodes': total_nodes, 'edges': total_edges})
+
+fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,4))
+pval_df.plot('pval', 'nodes', ax=ax1)
+pval_df.plot('pval', 'edges', ax=ax2)
+for pval in [0.1, 0.5, 0.8]:
+    ax1.axvline(pval, ls='--', color='k')
+    ax2.axvline(pval, ls='--', color='k')
+
+ax2.set_yscale('log')
+ax1.grid()
+ax2.grid()
+
+
+# Filtros
+pval_filt=0.1
+df_viz=df[df['pval']<=pval_filt]
+df_viz=df_viz[df_viz['source']!=df_viz['target']]
+df_viz
+
+
+# Creación de la red
+G=nx.from_pandas_edgelist(df_viz, 'source', 'target')
+
+# The network has disconnected components, let's check the size of each component in descending order
+compon=[len(c) for c in sorted(nx.connected_components(G), key=len, reverse=True)]
+compon
+print('Non connected: '+ str(sum(compon)-compon[0]))
+print('Connected nodes en la compoenente gigante: '+ str(compon[0]) +' de '+str(G.number_of_nodes())+' nodos totales.')
+
+print(compon)
+
+
+
+# Let's extract and use only the giant component
+S = [G.subgraph(c).copy() for c in nx.connected_components(G)]
+
+
+
+valores = [value[1] for value in grados_ordenados]
+
+fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+
+n, bins, patches = plt.hist(valores, bins=90, facecolor='#2ab0ff', edgecolor='#e0e0e0', linewidth=0.5, alpha=0.7)
+
+n = n.astype('int') # it MUST be integer
+# Good old loop. Choose colormap of your taste
+for i in range(len(patches)):
+    patches[i].set_facecolor(plt.cm.viridis(n[i]/max(n)))
+# Make one bin stand out   
+patches[47].set_alpha(1) # Set opacity
+# Add title and labels with custom font sizes
+plt.title('Distribución de grado de la Red ', fontsize=12)
+plt.xlabel('Grado', fontsize=20)
+plt.ylabel('Cantidad de Nodos', fontsize=20)
+ax.set_title("Distribución de Grado de la Red Filtrada por peso",
+             pad=24, fontweight=700, fontsize=20)
+
+
+# Filtered
+F = S[0]
+dict1=dict(F.degree())  # node 0 has degree 1
+sorted_dict = {}
+sorted_keys = sorted(dict1, key=dict1.get)  # [1, 3, 2]
+
+for w in sorted_keys:
+    sorted_dict[w] = dict1[w]
+values = list(sorted_dict.values())
+
+fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+
+n, bins, patches = plt.hist(values, bins=50, facecolor='#2ab0ff', edgecolor='#e0e0e0', linewidth=0.5, alpha=0.7)
+
+n = n.astype('int') # it MUST be integer
+# Good old loop. Choose colormap of your taste
+for i in range(len(patches)):
+    patches[i].set_facecolor(plt.cm.viridis(n[i]/max(n)))
+# Make one bin stand out   
+# patches[47].set_alpha(1) # Set opacity
+# Add title and labels with custom font sizes
+plt.title('Distribución de grado de la Red ', fontsize=12)
+plt.xlabel('Grado', fontsize=20)
+plt.ylabel('Cantidad de Nodos', fontsize=20)
+ax.set_title("Distribución de Grado de la Red Filtrada por pvalue",
+             pad=24, fontweight=700, fontsize=20)
+
+
+N10 = len(F)
+L10 = F.size()
+degrees10 = list(dict(F.degree()).values())
+kmin10 = min(degrees10)
+kmax10 = max(degrees10)
+print("Número de nodos: ", N10)
+print("Número de enlaces: ", L10)
+print('-------')
+print("Grado promedio: ", 2*L10/N10) 
+print('-------')
+print("Grado mínimo: ", kmin10)
+print("Grado máximo: ", kmax10)
+print('-------')
+print('Densidad: ', nx.density(F))
+print('Diametro: ',nx.diameter(F))
+
+nx.draw_networkx(F)
+
+
+d = {}
+for i, j in dict(nx.degree(F)).items():
+    if j in d:
+        d[j] += 1
+    else:
+        d[j] = 1
+x = np.log10(list((d.keys())))
+y = np.log10(list(d.values()))
+plt.scatter(x, y, alpha=0.9)
+plt.show()
+
+
+degree_g = dict(nx.degree(F))
+clustering_g=dict(nx.clustering(F))
+
+x = degree_g.values()
+y = clustering_g.values()
+
+
+# colors = np.random.rand(N)
+# area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
+
+plt.scatter(x, y, alpha=0.5)
+plt.show()
+
+
+
+import community.community_louvain as community_louvain
+
+partition = community_louvain.best_partition(F, random_state=5)
+
+# Rest of your code remains the same
+partition_df = pd.DataFrame(
+    data=partition.items(),
+    columns=['id', 'partition']
+)
+
+community_sizes = partition_df.groupby('partition')['id'].count().sort_values(ascending=False)
+
+print(f"Number of communities: {len(community_sizes)}")
+print("\nCommunity sizes:")
+print(community_sizes)
+
+
+plt.figure(figsize=(20, 10))
+#https://datoslab.cl/hes/
+
+partition = community_louvain.best_partition(F)
+
+size = (len(set(partition.values())))#Numero de comunidades
+print('Se detectan %d comunidades' % (size))
+
+pos = nx.spring_layout(F) # Layout para la red (coordenadas de los nodos y enlaces)
+
+count = 0
+colors = [np.array(cm.jet(x)).reshape(1,-1) for x in np.linspace(0, 1, size)]#cm.jet es el mapa de colores https://www.programcreek.com/python/example/56498/matplotlib.cm.jet
+for com in set(partition.values()): #para cada comunidad
+    list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]#guarda los personajes que pertenecen a la comunidad `com`
+    nx.draw_networkx_nodes(F, pos, list_nodes, node_size = 100, node_color=colors[count])#plotea nodos con colors por comunidad
+    count = count + 1# para iterar sobre los colores
+nx.draw_networkx_edges(F, pos, alpha=0.2)#plotea enlaces
+plt.show()
 
